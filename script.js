6518e6268def88e70e0dac34d47199f6c68ca6ca
Nabil <darknab@live.fr>
2022-12-19 18:23:29 +0100

    Created getUserSelection, works like a charm!

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let choiceNumber = Math.floor(Math.random() *3 );
     computerSelection = choices[choiceNumber];
-    console.log(computerSelection);
 }
-getComputerChoice();
 //create a function that asks for a user selection and checks if the selection is valid
+let userSelection;
+function getUserSelection() {
+    let validSelection = false; 
+    do {
+        userSelection = prompt("please enter your selection","");
+        if (userSelection === null) {
+            console.log("cancelled!");
+            return userSelection;
+        }
+        userSelection = userSelection.toLowerCase();
+        //check if selection is valid
+        switch (userSelection) {
+            case "rock" :
+            case "paper" :
+            case "scissors" :
+                validSelection = true;
+                console.log(validSelection);
+                return userSelection;          
+            break;
+            default:
+                alert("seriously?");
+                console.log(validSelection);
+        }
+    } 
+    while (validSelection === false) ;
+
+}
+
+
+getUserSelection();
+console.log(userSelection);
 // play 1 round
 //repeat 5 rounds
 //show results
