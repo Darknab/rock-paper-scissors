fc7d220d0816e1281380b98818ea429937c56b5f
Nabil <darknab@live.fr>
2022-12-20 10:02:04 +0100

    Created playRound and the loop that repeats the game 5 times, shows also the result

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let choiceNumber = Math.floor(Math.random() *3 );
     computerSelection = choices[choiceNumber];
+    return computerSelection;
 }
 //create a function that asks for a user selection and checks if the selection is valid
 let userSelection;
-function getUserSelection() {
+function getUserChoice() {
     let validSelection = false; 
     do {
         userSelection = prompt("please enter your selection","");
@@ -23,29 +24,89 @@ function getUserSelection() {
             case "paper" :
             case "scissors" :
                 validSelection = true;
-                console.log(validSelection);
                 return userSelection;          
             break;
             default:
                 alert("seriously?");
-                console.log(validSelection);
         }
     } 
     while (validSelection === false) ;
 
 }
-
-
-getUserSelection();
-console.log(userSelection);
 // play 1 round
-//repeat 5 rounds
-//show results
+let userScore = 0;
+let computerScore = 0;
+function playRound() {
+        getComputerChoice();
+        getUserChoice();
+        switch (userSelection) {
+            case null :
+                computerScore += 1;
+                console.log("you loose this round");
+                break;
+            case "rock":
+                if (computerSelection === "rock") {
+                    console.log("Draw");
+                }
+                else if (computerSelection === "paper") {
+                    computerScore += 1;
+                    console.log("Paper beats rock, you loose this round!");
+                }
+                else {
+                    userScore += 1;
+                    console.log("rock beats scissors, you win this round!")
+                }
+                break;
+            case "paper":
+                if (computerSelection === "paper") {
+                     console.log("Draw");
+                 }
+                 else if (computerSelection === "scissors") {
+                     computerScore += 1;
+                     console.log("scissors beat paper, you loose this round!");
+                  }
+                else {
+                    userScore += 1;
+                    console.log("paper beats rock, you win this round!")
+                   }
+                break;
+            case "scissors":
+                 if (computerSelection === "scissors") {
+                      console.log("Draw");
+                }
+                 else if (computerSelection === "rock") {
+                        computerScore += 1;
+                        console.log("rock beats scissors, you loose this round!");
+                    }
+                else {
+                    userScore += 1;
+                    console.log("scissors beats paper, you win this round!")
+                   }
+                break;         
+        }
 
     
+}
 
-   
+//repeat 5 rounds
+for (i = 0; i < 5; i++) {
+    console.log("round: " + (i + 1));
+    playRound();
+    console.log("your score is: " + userScore);
+    console.log("computer score is: " + computerScore);
+}
 
+//show results
+
+if (userScore > computerScore) {
+    console.log("congratulations, you won!");
+}
+else if (userScore < computerScore) {
+    console.log("shame on you! you lost against a stupid computer!");
+}
+else {
+    console.log("draw, you both rock!");
+}
 
 
 
