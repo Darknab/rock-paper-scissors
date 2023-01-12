f6cf9abff030260a861d01ce295a657e2aafb6a1
Nabil <darknab@live.fr>
2023-01-12 11:41:52 +0100

    The game stops when one player score 5 points and dislay Game over!

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-//create a function that make a random computer selection
+
 let computerSelection;
+
+let userScore = 0;
+let computerScore = 0;
+
+const user = document.querySelector(".user > .score");
+const uScore = document.createElement("p");
+
+const computer = document.querySelector(".computer > .score");
+const cScore = document.createElement("p");
+
+const board = document.querySelector(".buttons")
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
@@ -7,20 +18,8 @@ function getComputerChoice() {
     computerSelection = choices[choiceNumber];
     return computerSelection;
 }
-//create a function that asks for a user selection and checks if the selection is valid
-let userSelection;
-/*function getUserChoice() {
-    const buttons = document.querySelectorAll(".userChoice");
-    buttons.forEach((btn) =>{
-    btn.addEventListener("click", () => {
-        userSelection = btn.textContent;
-        playRound();
-        })
-    })
-}*/
-// play 1 round
-let userScore = 0;
-let computerScore = 0;
+
+
 function playRound() {
         getComputerChoice();
         /*getUserChoice();*/
@@ -68,57 +67,55 @@ function playRound() {
                     console.log("scissors beats paper, you win this round!")
                    }
                 break;         
-        }
-        user.appendChild(uScore);
-        uScore.textContent = userScore; 
-        computer.appendChild(cScore);
-        cScore.textContent = computerScore;     
+        }     
+
     
 }
 
 //Play a game until one player reach a score of 5
 function playGame() {
+    let maxScore = false;
+    
     const buttons = document.querySelectorAll(".userChoice");
     buttons.forEach((btn) =>{
     btn.addEventListener("click", () => {
         userSelection = btn.textContent;
         playRound();
+        scoreCheck();
         })
     })
-    if (userScore === 5 || computerScore ===5 ) {
-        gameOver();
-        return;
-    }
+    
 
 //show results
-
-if (userScore > computerScore) {
-    console.log("congratulations, you won!");
-}
-else if (userScore < computerScore) {
-    console.log("shame on you! you lost against a stupid computer!");
-}
-else {
-    console.log("draw, you both rock!");
+function showResults () {
+    if (userScore > computerScore) {
+        console.log("congratulations, you won!");
+    }
+    else if (userScore < computerScore) {
+        console.log("shame on you! you lost against a stupid computer!");
 }
 }
-const user = document.querySelector(".user > .score");
-const uScore = document.createElement("p");
-user.appendChild(uScore);
-uScore.textContent = userScore; 
 
-
-const computer = document.querySelector(".computer > .score");
-const cScore = document.createElement("p");
-computer.appendChild(cScore);
-cScore.textContent = computerScore; 
-
+ 
 function gameOver() {
     const results = document.querySelector(".results");
         const gameOver = document.createElement("h2");
         gameOver.textContent = "Game over!"
         results.appendChild(gameOver);
+        board.classList.toggle("inactive")
+        showResults()
 
+}
+
+function scoreCheck() {
+    let maxScore = false;
+    user.appendChild(uScore);
+    uScore.textContent = userScore; 
+    computer.appendChild(cScore);
+    cScore.textContent = computerScore;
+    if (userScore === 5 || computerScore === 5) {
+        gameOver();
+    }
 }
 
 
