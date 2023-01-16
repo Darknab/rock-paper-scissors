fa17099105dc4fcf541d1ef68383db92771c1210
Nabil <darknab@live.fr>
2023-01-16 15:46:13 +0100

    Corrected displaying issues

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 
 let computerSelection;
+let userSelection;
+
 
 let userScore = 0;
 let computerScore = 0;
+let round = 0;
 
 const user = document.querySelector(".user > .score");
 const uScore = document.createElement("p");
-
 const computer = document.querySelector(".computer > .score");
 const cScore = document.createElement("p");
-
+const scoreText = document.querySelector(".round");
+const roundNum = document.createElement("p");
+const roundResult = document.createElement("p");
+const winner = document.querySelector(".winner")
 const board = document.querySelector(".buttons");
 const startText = document.querySelector(".start > h3");
 const scores = document.querySelector(".scores");
 
 const results = document.querySelector(".results");
-const victory = document.createElement("p");
-victory.innerHTML = "Victory! <br> Congratulations, you won!";
-const defeat = document.createElement("p");
-defeat.innerHTML = "Defeat! <br> Shame on you! you lost against a stupid computer!!!";
-console.log(results)
+
 
 
 function getComputerChoice() {
@@ -32,95 +33,96 @@ function getComputerChoice() {
 
 function playRound() {
         getComputerChoice();
-        /*getUserChoice();*/
         switch (userSelection) {
-            case null :
-                computerScore += 1;
-                console.log("you loose this round");
-                break;
             case "rock":
                 if (computerSelection === "rock") {
-                    console.log("Draw");
+                    roundResult.innerHTML = `Draw!`
                 }
                 else if (computerSelection === "paper") {
                     computerScore += 1;
-                    console.log("Paper beats rock, you loose this round!");
+                    roundResult.innerHTML = `${computerSelection} beats ${userSelection}, you loose this round!`
                 }
                 else {
                     userScore += 1;
-                    console.log("rock beats scissors, you win this round!")
+                    roundResult.innerHTML = `${userSelection} beats ${computerSelection}, you win this round!`
                 }
                 break;
             case "paper":
                 if (computerSelection === "paper") {
-                     console.log("Draw");
-                 }
+                     roundResult.innerHTML = `Draw!`
+                    }
                  else if (computerSelection === "scissors") {
                      computerScore += 1;
-                     console.log("scissors beat paper, you loose this round!");
+                     roundResult.innerHTML = `${computerSelection} beats ${userSelection}, you loose this round!`
                   }
                 else {
                     userScore += 1;
-                    console.log("paper beats rock, you win this round!")
+                    roundResult.innerHTML = `${userSelection} beats ${computerSelection}, you win this round!`
                    }
                 break;
             case "scissors":
                  if (computerSelection === "scissors") {
-                      console.log("Draw");
-                }
+                      roundResult.innerHTML = `Draw!`
+                    }
                  else if (computerSelection === "rock") {
-                        computerScore += 1;
-                        console.log("rock beats scissors, you loose this round!");
+                    computerScore += 1;
+                    roundResult.innerHTML = `${computerSelection} beats ${userSelection}, you loose this round!`
                     }
                 else {
                     userScore += 1;
-                    console.log("scissors beats paper, you win this round!")
+                    roundResult.innerHTML = `${userSelection} beats ${computerSelection}, you win this round!`
                    }
                 break;         
-        }     
-    
+        } 
+        roundNum.textContent = `Round ${round}`;    
+        scoreText.appendChild(roundNum)
+        scoreText.appendChild(roundResult)
+        scoreCheck();
+
+}
+
+function setNewGame() {
+    computerScore = 0;
+    userScore = 0;
+    round = 0;
+    roundNum.innerHTML = "";
+    roundResult.innerHTML = "";
+    board.classList.remove("inactive");
+    startBtn.classList.add("inactive");
+    startText.classList.add("inactive");
+    scores.classList.remove("inactive");
+    results.classList.add("inactive");
 }
 
 //Play a game until one player reach a score of 5
 function playGame() {
-    board.classList.toggle("inactive");
-    startBtn.classList.toggle("inactive");
-    startText.classList.toggle("inactive");
-    scores.classList.toggle("inactive");
-    results.classList.add("inactive");
-    
-
-    const buttons = document.querySelectorAll(".userChoice");
-    buttons.forEach((btn) =>{
-    btn.addEventListener("click", () => {
-        userSelection = btn.textContent;
-        playRound();
-        scoreCheck();
+        const buttons = document.querySelectorAll(".userChoice");
+        buttons.forEach((btn) =>{
+        btn.addEventListener("click", () => {
+            userSelection = btn.textContent;
+            round ++;
+            playRound();
+            })
         })
-    })
-}    
+    
+}  
 
 //show results
 function showResults () {
     if (userScore > computerScore) {
-        results.appendChild(victory);
-        console.log(results)
+        winner.textContent = "Congratulations, you won!";
     }
     else if (userScore < computerScore) {
-        results.appendChild(defeat);
-        console.log(results)
+        winner.textContent = "Shame on you! you have lost against a stupid computer!";
     }
-    startBtn.classList.toggle("inactive");
 }
 
  
 function gameOver() {
-        results.classList.toggle("inactive")
-        const gameOver = document.createElement("h2");
-        gameOver.textContent = "Game over!"
-        results.appendChild(gameOver);
+        results.classList.toggle("inactive");
         board.classList.toggle("inactive");
         scores.classList.toggle("inactive");
+        startBtn.classList.toggle("inactive");
         showResults();
 
 }
@@ -137,7 +139,7 @@ function scoreCheck() {
 
 const startBtn = document.querySelector(".startbtn");
 startBtn.addEventListener("click", () => {
-    userScore = 0;
-    computerScore = 0;
+    setNewGame();
+    scoreCheck();
     playGame();
 });
